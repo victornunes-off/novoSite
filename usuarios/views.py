7fd64071fbceed95b89673e9da3a75062adf862a
Douglas Santana <douglassantanapassos7@gmail.com>
2025-06-13 22:04:32 -0400

    Aula: 13/06/2025

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from django.contrib.auth import login as login_django
+from django.contrib.auth import login as login_django, logout as logout_django
 
 def login(request):
     if request.method == "GET":
@@ -15,10 +15,17 @@ def login(request):
 
         if user:
             login_django(request, user)
-            return HttpResponse('Autenticado!')
+            return render(request, 'usuarios/home.html')
         else:
             return HttpResponse('E-mail ou senha inválidos!')
         
+def logout(request):
+    if request.user.is_authenticated:
+        logout_django(request)
+        return render(request, 'usuarios/login.html')
+    else:
+        return HttpResponse("Você não acessou sua conta ainda!")
+
 def cadastro(request):
     if request.method == 'GET':
         return render(request, 'usuarios/cadastro.html')
@@ -36,16 +43,28 @@ def cadastro(request):
             user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name)
             user.save()
 
-            return HttpResponse("Usuário cadastrado com sucesso!")
+            return render(request, 'Usuarios/login.html')
 
 def home(request):
-    return render(request, 'usuarios/home.html')
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/home.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
 
 def lancar(request):
-    return render(request, 'usuarios/lancar.html')
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/lancar.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
 
 def alterar(request):
-    return render(request, 'usuarios/alterar.html')
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/alterar.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
 
 def visualizar(request):
-    return render(request, 'usuarios/visualizar.html')
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/visualizar.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
