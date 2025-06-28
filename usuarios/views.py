4c8bbbf320901a84a19df6a455f94202509e8a4b
Douglas Santana <douglassantanapassos7@gmail.com>
2025-06-28 17:19:08 -0400

    AULA 28/06/25

--- usuarios/views.py
+++ usuarios/views.py
@@ -82,7 +82,20 @@ def alterar(request):
         return HttpResponse("Faça o login para acessar!")
 
 def visualizar(request):
-    if request.user.is_authenticated:
-        return render(request, 'usuarios/visualizar.html')
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            lista_notas = Nota.objects.all()
+            dicionario_notas = {'lista_notas': lista_notas}
+            return render(request, 'usuarios/visualizar.html', dicionario_notas)
+        else:
+            return HttpResponse("Faça o login para acessar!")
     else:
-        return HttpResponse("Faça o login para acessar!")
+        disciplina = request.POST.get('disciplina')
+        if disciplina == "Todas as disciplinas":
+            lista_notas = Nota.objects.all()
+            dicionario_notas = {'lista_notas': lista_notas}
+            return render(request, 'usuarios/visualizar.html', dicionario_notas)
+        else:
+            lista_notas = Nota.objects.filter(disciplina=disciplina)
+            dicionario_notas_filtradas = {'lista_notas': lista_notas}
+            return render(request, 'usuarios/visualizar.html', dicionario_notas_filtradas)
