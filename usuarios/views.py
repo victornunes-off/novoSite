01c8575d6ffec788a46cc4e32c689aaa09b56c46
Douglas Santana <douglassantanapassos7@gmail.com>
2025-07-05 11:07:20 -0400

    Atualização 04.07.2025

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django, logout as logout_django
@@ -76,10 +77,13 @@ def lancar(request):
             return render(request, 'usuarios/home.html')
 
 def alterar(request):
-    if request.user.is_authenticated:
-        return render(request, 'usuarios/alterar.html')
-    else:
-        return HttpResponse("Faça o login para acessar!")
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            lista_notas = Nota.objects.all()
+            dicionario_notas = {'lista_notas':lista_notas}
+            return render(request, 'usuarios/alterar.html', dicionario_notas)
+        else:
+            return HttpResponse("Faça o login para acessar!")
 
 def visualizar(request):
     if request.method == "GET":
@@ -99,3 +103,21 @@ def visualizar(request):
             lista_notas = Nota.objects.filter(disciplina=disciplina)
             dicionario_notas_filtradas = {'lista_notas': lista_notas}
             return render(request, 'usuarios/visualizar.html', dicionario_notas_filtradas)
+        
+def excluir_verificacao(request, pk):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            lista_notas = Nota.objects.get(pk=pk)
+            dicionario_notas = {'lista_notas': lista_notas}
+            return render(request, 'usuarios/excluir.html', dicionario_notas)
+        else:
+            return HttpResponse("Faça o login para acessar!")
+
+def excluir(request, pk):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            disciplina_selecionada = Nota.objects.get(pk=pk)
+            disciplina_selecionada.delete()
+            return HttpResponseRedirect(reverse('alterar'))
+        else:
+            return HttpResponse("Faça o login para acessar!")
